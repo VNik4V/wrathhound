5f9be8018cddbe8748c531c9f09d902a04f9f00d
diak <diak@mesterlevel.lo>
2025-01-31 10:26:00 +0100

    a

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const {PORT,HOST} = require('./config/config').config;
+const {PORT} = require('./config/config').config;
 
-app.listen(PORT,HOST, ()=>{
-    console.log(`http://${HOST}:${PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`port: ${PORT}`);
 });
