b98d1f9a3e0d1f159033743b20a6b8a6e32d3de8
diak <diak@mesterlevel.lo>
2025-02-20 12:08:36 +0100

    más barátainak listája

--- modules/friends/friends.controller.js
+++ modules/friends/friends.controller.js
@@ -86,6 +86,30 @@ const allFriends = async (req,res) => {
     */
 };
 
+const someoneFriends = async (req,res) => {
+    const {uid} = req.params;
+    try {
+        const [result] = await db.query(sql.friends, [uid]);
+        if(result.length === 0){
+            return res.status(200).json({message: 'A barátlista üres'});
+        }
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(500).json({error: 'Hiba a mátrixban'});
+    }
+    /*
+    db.query(sql.friends, [id], (err, result) => {
+        if(err){
+            return res.status(500).json({error: 'Hiba a mátrixban'});
+        }
+        if(result.length === 0){
+            return res.status(200).json({message: 'A barátlista üres'});
+        }
+        return res.status(200).json(result);
+    });
+    */
+};
+
 const removeFriend = async (req,res) => {
     const {id} = req.user;
     const {uid} = req.params;
@@ -112,4 +136,4 @@ const removeFriend = async (req,res) => {
 };
 
 
-module.exports = {addFriend, pending, allFriends, removeFriend}
+module.exports = {addFriend, pending, allFriends, removeFriend, someoneFriends}
--- modules/friends/friends.router.js
+++ modules/friends/friends.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addFriend, pending, allFriends, removeFriend} = require('./friends.controller');
+const { addFriend, pending, allFriends, removeFriend, someoneFriends} = require('./friends.controller');
 const {authenticateToken} = require('../../middleware/auth.middleware');
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/addfriend/:uid', authenticateToken, addFriend);
 router.get('/pending', authenticateToken, pending);
 router.get('/all', authenticateToken, allFriends);
+router.get('/all/:uid', someoneFriends);
 router.delete('/removefriend/:uid', authenticateToken, removeFriend);
 
 module.exports = router;
